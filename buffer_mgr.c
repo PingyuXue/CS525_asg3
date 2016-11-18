14818e3f797891e6b35c12f399164176b7be6d27
PingyuXue <1992x520py@gmail.com>
2016-11-17 22:09:18 -0600

    Add files via upload

--- buffer_mgr.c
+++ buffer_mgr.c
@@ -4,59 +4,11 @@
 #include "dberror.h"
 #include "storage_mgr.h"
 
-/*
- // Replacement Strategies
-typedef enum ReplacementStrategy {
-  RS_FIFO = 0,
-  RS_LRU = 1,
-  RS_CLOCK = 2,
-  RS_LFU = 3,
-  RS_LRU_K = 4
-} ReplacementStrategy;
-
-// Data Types and Structures
-typedef int PageNumber;
-#define NO_PAGE -1
-
-typedef struct BM_BufferPool {
-  char *pageFile;
-  int numPages;
-  ReplacementStrategy strategy;
-  void *mgmtData; // use this one to store the bookkeeping info your buffer
-                  // manager needs for a buffer pool
-  int numReadIO;
-  int numWriteIO;
-  int timer;
-} BM_BufferPool;
-
-typedef struct BM_PageHandle {
-  PageNumber pageNum;
-  char *data;
-  bool dirty;
-  int fixCounts;
-  void* strategyAttribute;
-} BM_PageHandle;
-
- */
-
 
 // Buffer Manager Interface Pool Handling
 
 /***************************************************************
  * Function Name: initBufferPool
- *
- * Description: initBufferPool creates a new buffer pool with numPages page frames using the page replacement strategy strategy. The pool is used to cache pages from the page file with name pageFileName. Initially, all page frames should be empty. The page file should already exist, i.e., this method should not generate a new page file. stratData can be used to pass parameters for the page replacement strategy. For example, for LRU-k this could be the parameter k.
- *
- * Parameters: BM_BufferPool *const bm, const char *const pageFileName, const int numPages, ReplacementStrategy strategy, void *stratData.
- *
- * Return: RC
- *
- * Author: Xiaoliang Wu
- *
- * History:
- *      Date            Name                        Content
- *      16/02/24        Xiaoliang Wu                Not init pageHandle.
- *  02/27/16        Zhipeng Liu         add some init
 ***************************************************************/
 
 RC initBufferPool(BM_BufferPool *const bm, const char *const pageFileName,
@@ -83,6 +35,9 @@ RC initBufferPool(BM_BufferPool *const bm, const char *const pageFileName,
     return RC_OK;
 }
 
+/***************************************************************
+ * Function Name: initPages
+ ***************************************************************/
 RC initPages(BM_BufferPool *const bm, const int numPages) {
     int i;
     for (i = 0; i < numPages; i++)
@@ -97,21 +52,6 @@ RC initPages(BM_BufferPool *const bm, const int numPages) {
 
 /***************************************************************
  * Function Name: shutdownBufferPool
- *
- * Description: shutdownBufferPool destroys a buffer pool. This method should free up all resources associated with buffer pool. For example, it should free the memory allocated for page frames. If the buffer pool contains any dirty pages, then these pages should be written back to disk before destroying the pool. It is an error to shutdown a buffer pool that has pinned pages.
- *
- * Parameters: BM_BufferPool *const bm
- *
- * Return: RC
- *
- * Author: Xiaoliang Wu
- *
- * History:
- *      Date            Name                        Content
- *      16/02/24        Xiaoliang Wu                Complete.
- *      16/02/26        Xiaoliang Wu                Free buffer in pages.
- *      16/02/27        Xincheng Yang               Free fixCounts.
- *
 ***************************************************************/
 
 
@@ -143,20 +83,6 @@ RC shutdownBufferPool(BM_BufferPool *const bm) {
 
 /***************************************************************
  * Function Name: forceFlushPool
- *
- * Description: forceFlushPool causes all dirty pages (with fix count 0) from the buffer pool to be written to disk.
- *
- * Parameters: BM_BufferPool *const bm
- *
- * Return: RC
- *
- * Author: Xiaoliang Wu
- *
- * History:
- *      Date            Name                        Content
- *      16/02/25        Xiaoliang Wu                Complete, forcepage need set dirty to 0.
- *      16/02/27        Xincheng Yang               free fixCounts and dirtyFlags.
- *
 ***************************************************************/
 
 RC forceFlushPool(BM_BufferPool *const bm) {
@@ -194,18 +120,6 @@ RC forceFlushPool(BM_BufferPool *const bm) {
 
 /***************************************************************
  * Function Name: markDirty
- *
- * Description:mark a page as dirty
- *
- * Parameters: BM_BufferPool *const bm, BM_PageHandle *const page
- *
- * Return:RC
- *
- * Author:Zhipeng Liu
- *
- * History:
- *      Date            Name                        Content
- *      02/25/16        Zhipeng Liu                 complete
 ***************************************************************/
 
 int seekPage (BM_BufferPool *const bm, BM_PageHandle *const page) {
@@ -230,18 +144,6 @@ RC markDirty (BM_BufferPool *const bm, BM_PageHandle *const page)
 
 /***************************************************************
  * Function Name: unpinPage
- *
- * Description:unpin a page indicate by "page"
- *
- * Parameters:BM_BufferPool *const bm, BM_PageHandle *const page
- *
- * Return:RC
- *
- * Author:Zhipeng Liu
- *
- * History:
- *      Date            Name                        Content
- *      02/25/16        Zhipeng Liu                 complete
 ***************************************************************/
 
 RC unpinPage (BM_BufferPool *const bm, BM_PageHandle *const page)
@@ -253,18 +155,6 @@ RC unpinPage (BM_BufferPool *const bm, BM_PageHandle *const page)
 
 /***************************************************************
  * Function Name: forcePage
- *
- * Description:write the current content of the page back to the page file on disk
- *
- * Parameters:BM_BufferPool *const bm, BM_PageHandle *const page
- *
- * Return:RC
- *
- * Author:Zhipeng Liu
- *
- * History:
- *      Date            Name                        Content
- *  02/16/2016  Zhipeng Liu        finish the function
 ***************************************************************/
 
 RC forcePage (BM_BufferPool *const bm, BM_PageHandle *const page)
@@ -287,18 +177,6 @@ RC forcePage (BM_BufferPool *const bm, BM_PageHandle *const page)
 
 /***************************************************************
  * Function Name: pinPage
- *
- * Description:pin a page, if the page does not exist in memory, read it from file
- *
- * Parameters:BM_BufferPool *const bm, BM_PageHandle *const page, const PageNumber pageNum
- *
- * Return:RC
- *
- * Author:Zhipeng Liu
- *
- * History:
- *      Date            Name                        Content
- *02/25/16       Zhipng Liu             imcomplete, need to implement the replace page part
 ***************************************************************/
 
 RC pinPage (BM_BufferPool *const bm, BM_PageHandle *const page,
@@ -310,7 +188,9 @@ RC pinPage (BM_BufferPool *const bm, BM_PageHandle *const page,
     int i = 0;
     while (i < bm->numPages)
     {
-        if ((bm->mgmtData + i)->pageNum == -1)
+        int transNum = (bm->mgmtData + i)->pageNum;
+//        if ((bm->mgmtData + i)->pageNum == -1)
+        if (transNum == -1)
         {
             (bm->mgmtData + i)->data = (char*)calloc(PAGE_SIZE, sizeof(char));
             pnum = i;
@@ -318,7 +198,8 @@ RC pinPage (BM_BufferPool *const bm, BM_PageHandle *const page,
             break;
         }
         
-        if ((bm->mgmtData + i)->pageNum == pageNum)
+//        if ((bm->mgmtData + i)->pageNum == pageNum)
+        if (transNum == pageNum)
         {
             pnum = i;
             flag = 2;
@@ -331,50 +212,105 @@ RC pinPage (BM_BufferPool *const bm, BM_PageHandle *const page,
         {
 
             flag = 1;
-            if (bm->strategy == RS_FIFO)
-            {
-                pnum = strategyFIFOandLRU(bm);
-                if ((bm->mgmtData + pnum)->dirty)
-                    forcePage (bm, bm->mgmtData + pnum);
+            // change to switch
+            ReplacementStrategy transStrat = bm->strategy;
+            switch ( transStrat) {
+                case RS_FIFO:
+                {
+                    pnum = strategyFIFOandLRU(bm);
+                    
+                    if ((bm->mgmtData + pnum)->dirty)
+                        forcePage (bm, bm->mgmtData + pnum);
+                    break;
+                }
+                    
+                case  RS_LRU:
+                {
+                    pnum = strategyFIFOandLRU(bm);
+                    if ((bm->mgmtData + pnum)->dirty)
+                        forcePage (bm, bm->mgmtData + pnum);
+                    break;
+                    
+                }
+                    
+                default:
+                    break;
             }
-            if (bm->strategy == RS_LRU)
-            {
-                pnum = strategyFIFOandLRU(bm);
-                if ((bm->mgmtData + pnum)->dirty)
-                    forcePage (bm, bm->mgmtData + pnum);
-            }
+//            if (bm->strategy == RS_FIFO)
+//            {
+//                pnum = strategyFIFOandLRU(bm);
+//                if ((bm->mgmtData + pnum)->dirty)
+//                    forcePage (bm, bm->mgmtData + pnum);
+//            }
+//            if (bm->strategy == RS_LRU)
+//            {
+//                pnum = strategyFIFOandLRU(bm);
+//                if ((bm->mgmtData + pnum)->dirty)
+//                    forcePage (bm, bm->mgmtData + pnum);
+//            }
         }
         i++;
     }
     
-    if (flag == 1)
-    {
-        FILE* fp;
-        fp = fopen(bm->pageFile, "r");
-        fseek(fp, pageNum * PAGE_SIZE, SEEK_SET);
-        fread((bm->mgmtData + pnum)->data, sizeof(char), PAGE_SIZE, fp);
-        page->data = (bm->mgmtData + pnum)->data;
-        bm->numRead++;
-        ((bm->mgmtData + pnum)->fixCount)++;
-        (bm->mgmtData + pnum)->pageNum = pageNum;
-        page->fixCount = (bm->mgmtData + pnum)->fixCount;
-        page->pageNum = pageNum;
-        page->dirty = (bm->mgmtData + pnum)->dirty;
-        page->strategyType = (bm->mgmtData + pnum)->strategyType;
-        updataAttribute(bm, bm->mgmtData + pnum);
-        fclose(fp);
-    }
-    if (flag == 2)
-    {
-        page->data = (bm->mgmtData + pnum)->data;
-        ((bm->mgmtData + pnum)->fixCount)++;
-        page->fixCount = (bm->mgmtData + pnum)->fixCount;
-        page->pageNum = pageNum;
-        page->dirty = (bm->mgmtData + pnum)->dirty;
-        page->strategyType = (bm->mgmtData + pnum)->strategyType;
-        //if(bm->strategy==RS_LRU)
-        //  updataAttribute(bm, bm->mgmtData+pnum);
-    }
+//     change to switch
+        switch (flag){
+            case 1:
+            {
+                FILE* fp;
+                fp = fopen(bm->pageFile, "r");
+                fseek(fp, pageNum * PAGE_SIZE, SEEK_SET);
+                fread((bm->mgmtData + pnum)->data, sizeof(char), PAGE_SIZE, fp);
+                page->data = (bm->mgmtData + pnum)->data;
+                bm->numRead++;
+                ((bm->mgmtData + pnum)->fixCount)++;
+                (bm->mgmtData + pnum)->pageNum = pageNum;
+                page->fixCount = (bm->mgmtData + pnum)->fixCount;
+                page->pageNum = pageNum;
+                page->dirty = (bm->mgmtData + pnum)->dirty;
+                page->strategyType = (bm->mgmtData + pnum)->strategyType;
+                updataAttribute(bm, bm->mgmtData + pnum);
+                fclose(fp);
+                break;
+            }
+                case 2:
+            {
+                page->data = (bm->mgmtData + pnum)->data;
+                ((bm->mgmtData + pnum)->fixCount)++;
+                page->fixCount = (bm->mgmtData + pnum)->fixCount;
+                page->pageNum = pageNum;
+                page->dirty = (bm->mgmtData + pnum)->dirty;
+                page->strategyType = (bm->mgmtData + pnum)->strategyType;
+                break;
+            }
+        }
+//    if (flag == 1)
+//    {
+//        FILE* fp;
+//        fp = fopen(bm->pageFile, "r");
+//        fseek(fp, pageNum * PAGE_SIZE, SEEK_SET);
+//        fread((bm->mgmtData + pnum)->data, sizeof(char), PAGE_SIZE, fp);
+//        page->data = (bm->mgmtData + pnum)->data;
+//        bm->numRead++;
+//        ((bm->mgmtData + pnum)->fixCount)++;
+//        (bm->mgmtData + pnum)->pageNum = pageNum;
+//        page->fixCount = (bm->mgmtData + pnum)->fixCount;
+//        page->pageNum = pageNum;
+//        page->dirty = (bm->mgmtData + pnum)->dirty;
+//        page->strategyType = (bm->mgmtData + pnum)->strategyType;
+//        updataAttribute(bm, bm->mgmtData + pnum);
+//        fclose(fp);
+//    }
+//    if (flag == 2)
+//    {
+//        page->data = (bm->mgmtData + pnum)->data;
+//        ((bm->mgmtData + pnum)->fixCount)++;
+//        page->fixCount = (bm->mgmtData + pnum)->fixCount;
+//        page->pageNum = pageNum;
+//        page->dirty = (bm->mgmtData + pnum)->dirty;
+//        page->strategyType = (bm->mgmtData + pnum)->strategyType;
+//        //if(bm->strategy==RS_LRU)
+//        //  updataAttribute(bm, bm->mgmtData+pnum);
+//    }
     return RC_OK;
 }
 
